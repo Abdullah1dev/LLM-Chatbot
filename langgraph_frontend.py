bdde229cd797d460ba3271c272ca556e4cb00eaf
Abdullah1dev <242677@students.au.edu.pk>
2026-06-29 11:39:47 +0500

    Create a sidebar and add the functionality of dynamic thread id for new chat

--- langgraph_frontend.py
+++ langgraph_frontend.py
@@ -1,11 +1,43 @@
 import streamlit as st
 from langgraph_backend import workflow
-user_input = st.chat_input("Type Here")
 from langchain_core.messages import HumanMessage
+import uuid
 
-Config = {'configurable' : {'thread_id' : 'thread-1'}}
+
+user_input = st.chat_input("Type Here")
+
+
+#utitlity funtion
+
+def generate_threadid():
+    thread_id = uuid.uuid4()
+    return thread_id
+
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_threadid()
+
+
+
+
+config = {'configurable' : {'thread_id' : st.session_state['thread_id']}}
+
+
+    
+    
+    
+st.sidebar.title('LangGraph Chatbot')
+
+st.sidebar.button('New Chat')
+
+st.sidebar.header('My Conversations')
+
+st.sidebar.text(st.session_state['thread_id'])
+
+
 
 
 
@@ -33,15 +65,10 @@ if user_input:
         ai_message = st.write_stream(
             message_chunk.content for message_chunk , metadata in workflow.stream(
             {'messages' : [HumanMessage(content=user_input)]},
-            config = {'configurable' : {'thread_id' : 'thread-1'}},
+            config = config,
             stream_mode = 'messages'
         )
         )
         
     st.session_state['message_history'].append({'role' : 'assistant' , 'content' : ai_message})
-       
-        
-        
-        
-        
-        
+     
