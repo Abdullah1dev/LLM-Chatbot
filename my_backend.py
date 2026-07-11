835aea5d248077b0dcb37a479717b24266645e5a
Abdullah1dev <242677@students.au.edu.pk>
2026-07-11 12:23:44 +0500

    Create Seprate folder for RAG and move all the logic of RAG to it

--- my_backend.py
+++ my_backend.py
@@ -5,10 +5,9 @@ import os
 
 from langchain_core.messages import HumanMessage
 
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
+
+
+
 from langchain_core.tools import tool
 from langgraph.prebuilt import ToolNode
 from langgraph.prebuilt import tools_condition
@@ -16,59 +15,11 @@ from src.llm import model
 from src.memory import checkpointer , retrieve_all_threads
 from src.state import ChatState
 
+from src.rag import embeddings , initialize_rag , set_retriever , retriever
 
-
-embeddings = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5"
-)
-
-retriever = None
-
-def set_retriever(new_retriever):
-    global retriever
-    retriever = new_retriever
     
 
-def initialize_rag(uploaded_file):
-    os.makedirs("temp/" , exist_ok=True)
-    
-    try:
-        
-        pdf_path = os.path.join("temp/" , uploaded_file.name)
-         
-        with open(pdf_path , 'wb') as f:
-            f.write(uploaded_file.getvalue())
-            
-        
-        loader = PyPDFLoader(pdf_path)
-        
-        documents = loader.load()
-        
-        text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 100
-        )
-        
-        chunks = text_splitter.split_documents(documents)
-        
-        vector_store = FAISS.from_documents(chunks , embeddings)
-        
-        retriever = vector_store.as_retriever(
-        search_type = 'similarity',
-        search_kwargs = {"k" : 4}
-        
-        )
-        if os.path.exists(pdf_path):
-            os.remove(pdf_path)
-            
-        
-        
-        return retriever 
-    
-    except Exception as e:
-        raise e
-    
-    
+
 
 
 @tool
---
+++ src/rag.py
@@ -0,0 +1,64 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+retriever = None
+
+
+
+
+
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-small-en-v1.5"
+)
+
+
+def set_retriever(new_retriever):
+    global retriever
+    retriever = new_retriever
+    
+    
+
+def initialize_rag(uploaded_file):
+    os.makedirs("temp/" , exist_ok=True)
+    
+    try:
+        
+        pdf_path = os.path.join("temp/" , uploaded_file.name)
+         
+        with open(pdf_path , 'wb') as f:
+            f.write(uploaded_file.getvalue())
+            
+        
+        loader = PyPDFLoader(pdf_path)
+        
+        documents = loader.load()
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 100
+        )
+        
+        chunks = text_splitter.split_documents(documents)
+        
+        vector_store = FAISS.from_documents(chunks , embeddings)
+        
+        retriever = vector_store.as_retriever(
+        search_type = 'similarity',
+        search_kwargs = {"k" : 4}
+        
+        )
+        if os.path.exists(pdf_path):
+            os.remove(pdf_path)
+            
+        
+        
+        return retriever 
+    
+    except Exception as e:
+        raise e
+    
+    
